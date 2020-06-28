32bf6698304829ca1fa147863594321e6ab271d3
David Peter Hansen <davidph@microsoft.com>
2020-06-27 21:10:09 -0400

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -170,7 +170,7 @@ brew cask install --appdir="/Applications" grammarly
 brew cask install --appdir="/Applications" prowritingaid
 brew cask install --appdir="/Applications" istat-menus
 brew cask install --appdir="/Applications" rsyncosx
-brew cask install --appdir="/Applications" fanny
+brew cask install --appdir="/Applications" macs-fan-control
 
 # Install Docker, which requires virtualbox
 #brew install docker
